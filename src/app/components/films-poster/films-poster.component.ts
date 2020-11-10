4d2ad8b62c835f11f33e697fcb76b84b8fa63732
Miguel Angel <miguelangelteletor@yahoo.es>
2020-11-10 17:13:53 +0100

    feat: add some thing

--- src/app/components/films-poster/films-poster.component.ts
+++ src/app/components/films-poster/films-poster.component.ts
@@ -1,53 +1,22 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
-
-import Swiper from 'swiper';
+import { Component, Input, OnInit } from '@angular/core';
+import { FilmsResponse } from 'src/app/interface/films-response';
+import { FilmsService } from 'src/app/services/films.service';
+import { StarRatingComponent } from 'ng-starrating';
 
 @Component({
   selector: 'app-films-poster',
   templateUrl: './films-poster.component.html',
   styleUrls: ['./films-poster.component.css'],
 })
-export class FilmsPosterComponent implements OnInit, AfterViewInit {
+export class FilmsPosterComponent implements OnInit {
+  // @Input() films: any;
   @Input() coming: any;
-  // public coming: any;
-  public mySwiper: Swiper;
 
-  constructor() {}
-  ngAfterViewInit(): void {
-    this.mySwiper = new Swiper('.swiper-container', {
-      // Optional parameters
-      // direction: 'vertical',
-      loop: true,
-
-      // If we need pagination
-      // pagination: {
-      //   el: '.swiper-pagination',
-      // },
-
-      // // Navigation arrows
-      // navigation: {
-      //   nextEl: '.swiper-button-next',
-      //   prevEl: '.swiper-button-prev',
-      // },
-
-      // // And if we need scrollbar
-      // scrollbar: {
-      //   el: '.swiper-scrollbar',
-      // },
-    });
-    // mySwiper.slideNext();
-  }
+  constructor(private filmService: FilmsService) {}
 
   ngOnInit(): void {
-    // this.filmService.getUpcomming().subscribe((resultUpcomming: any) => {
-    //   this.coming = resultUpcomming.results;
-    //   console.log('look the popular', this.coming);
+    //  this.filmService.getUpcomming().subscribe((result) => {
+    // //   console.log('look again', result);
     // });
-  }
-  slideNextt() {
-    this.mySwiper.slideNext();
-  }
-  slidePrevv() {
-    this.mySwiper.slidePrev();
   }
 }
